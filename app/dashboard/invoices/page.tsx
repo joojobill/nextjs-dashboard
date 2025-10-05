6aa22e47dca2529fe320dd43a802eeda4465388e
joojobill <idhamzzyisaac@gmail.com>
2025-10-05 14:44:46 +0000

    week 3

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,11 +3,8 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
- 
 export default async function Page(props: {
     searchParams?: {
         query?: string;
@@ -19,7 +16,6 @@ export default async function Page(props: {
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
-
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -29,11 +25,9 @@ export default async function Page(props: {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense>}
+      <Table query={query} currentPage={currentPage} />
       <div className="mt-5 flex w-full justify-center">
-        {<Pagination totalPages={totalPages} />}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
